17bbdba28890d38c117109eb010f337e37fb3e0a
Ettore <ettore.contas@gmail.com>
2021-07-03 18:55:36 -0300

    create repository

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ApplicationRecord
+  belongs_to :author
+  has_many :elements
+
+  has_one_attached :header_image
+
+  scope :published, -> do
+    where(published: true)
+  end
+
+  scope :most_recently_published,  -> do
+    order(published_at: :desc)
+  end
+end
